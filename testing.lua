8c82b01d01e4e88273d7d8d2c13436cc99ffbf7a
Nev <153566382+outrozl@users.noreply.github.com>
2024-02-21 19:04:34 -0600

    Update testing.lua

--- testing.lua
+++ testing.lua
@@ -1,1 +1,46 @@
 
+-- NEW UI TEST
+
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+
+local function GetVer()
+    local versionURL = "https://raw.githubusercontent.com/outrozl/project/main/nevscripthub/scripts/tools/version.txt"
+    
+    local response = game.HttpGet(versionURL)
+    
+    if response then
+        local NevVer = response:match("(%d+%.%d+%.%d+)")
+        return NevVer
+    else
+        return nil
+    end
+end
+
+-- Get Version
+local NevVer = GetVer()
+
+local Window = OrionLib:MakeWindow({Name = "Nev Script Hub | "..NevVer, HidePremium = false, SaveConfig = false, IntroEnabled = true, IntroText = "Nev Studios", IntroIcon = "rbxassetid://6034227139", ConfigFolder = "NevTesting"})
+
+-- Tabs
+
+local Tab = Window:MakeTab({
+	Name = "Config",
+	Icon = "rbxassetid://6031280894",
+	PremiumOnly = false
+})
+
+-- Content
+
+
+-- UI Loaded notify
+
+OrionLib:MakeNotification({
+    Name = "Nev | Script Hub | "..NevVer,
+    Content = "Script loaded successfully!",
+    Image = "rbxassetid://4483345998",
+    Time = 5
+})
+
+-- Init
+
+OrionLib:Init()
